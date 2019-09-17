7822bc5f69ba259e7ac321a402081057e2f6af76
Boat Piyawat <Botba109@gmail.com>
2019-09-17 11:39:15 +0700

    lab8_person_EMP&APP

---
+++ src/lab8/EmployeeApp.java
@@ -0,0 +1,31 @@
+package lab8;
+
+public class EmployeeApp {
+    public static void main(String[] args) {
+
+        Employee emp1 = new Employee("bot","001","EMP001");
+        System.out.println(emp1.toString());
+
+        Person p1 = new Employee("piyawat","002","EMP002");
+        System.out.println(p1.toString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+}
